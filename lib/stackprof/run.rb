269eb7bc5a0d7a885ef5fd1e62c0deaf6c136715
Masataka Kuwabara <p.ck.t22@gmail.com>
2017-01-03 15:27:19 +0900

    bunlde gem

---
+++ lib/stackprof/run.rb
@@ -0,0 +1,7 @@
+require "stackprof/run/version"
+
+module Stackprof
+  module Run
+    # Your code goes here...
+  end
+end
---
+++ lib/stackprof/run/version.rb
@@ -0,0 +1,5 @@
+module Stackprof
+  module Run
+    VERSION = "0.1.0"
+  end
+end
